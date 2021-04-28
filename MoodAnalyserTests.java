0ebcb7e712d7cc8d5f0f6f2e82b156e6d2ac6329
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-04-28 06:59:45 +0530

    Add:Message should return sad

---
+++ MoodAnalyserTests.java
@@ -0,0 +1,18 @@
+package com.moodAnalyzer;
+import static org.junit.Assert.*;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+public class MoodAnalyserTests {
+	
+	@Test
+    public void testMoodAnalyzer_Constructor_SadMessage_shouldReturn_SAD()
+    {	
+		String mood = MoodAnalyzer.MoodAnalyzer();
+		System.out.println(mood);
+        assertEquals("SAD",mood);
+    }
+    
+}
+
+
---
+++ MoodAnalyzer.java
@@ -0,0 +1,14 @@
+package com.moodAnalyzer;
+
+public class MoodAnalyzer {
+
+	private static String message;
+	public static String MoodAnalyzer() {
+		return "SAD";
+	}
+	
+	public static String MoodAnalyzer(String message) {
+		return "Sad";
+	}
+
+}
