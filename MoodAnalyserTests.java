089e7dce6e3656cbe6742ee934524a51abfaa85e
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-04-28 07:13:45 +0530

    Add:message happy mood added

--- MoodAnalyserTests.java
+++ MoodAnalyserTests.java
@@ -3,16 +3,16 @@ import static org.junit.Assert.*;
 
 import org.hamcrest.CoreMatchers;
 import org.junit.Test;
-public class MoodAnalyserTests {
-	
-	@Test
-    public void testMoodAnalyzer_Constructor_SadMessage_shouldReturn_SAD()
-    {	
-		String mood = MoodAnalyzer.MoodAnalyzer();
-		System.out.println(mood);
-        assertEquals("SAD",mood);
+public class MoodAnalyserTests 
+{
+
+    @Test
+    public void testMoodAnalyzer_anyMOod_shouldReturn_Happy()
+    {
+    	String mood=moodanalyzer.analyseMood("I am in Any Mood");
+	System.out.println(mood);
+        assertEquals("HAPPY",mood);
     }
-    
 }
 
 
